98b628a9c78b24886be38525c2466fc3eaae7c9c
Carlos Vicente <carlosvchrdevelop@gmail.com>
2021-02-13 00:55:15 +0100

    Added two buttons to control resume and restart in order to make it easy the user interaction. Improved appearance.

--- components/Chrono.styles.ts
+++ components/Chrono.styles.ts
@@ -1,20 +1,21 @@
 import { Dimensions, StyleSheet } from 'react-native';
 
-const dimension = Math.min(Dimensions.get('screen').height, Dimensions.get('screen').width);
+export const dimension = Math.min(Dimensions.get('screen').height, Dimensions.get('screen').width);
 export const primaryColor = '#424242';
 export const secondaryColor = '#F5F5F5';
+const resetColor = '#FF5A00';
+const resumeColor = '#1D2F43';
 
 const styles = StyleSheet.create({
     chronoContainer: {
         alignContent: 'center',
         alignItems: 'center',
         backgroundColor: '#ffffff',
-        borderWidth: 2,
-        borderColor: primaryColor,
         borderRadius: dimension,
         height: dimension * 1.08,
         justifyContent: 'center',
-        width: dimension * 1.08
+        width: dimension * 1.08,
+        elevation: 4,
     },
     chronoText: {
         color: primaryColor,
@@ -26,6 +27,29 @@ const styles = StyleSheet.create({
         fontSize: 24,
         marginLeft: 6,
         textAlign: 'center'
+    },
+    resumeResetContainer: {
+        backgroundColor: '#ffffff',
+        borderRadius: dimension,
+        display: 'flex',
+        elevation: 8,
+        flexDirection: 'row',
+        padding: 4,
+        position: 'absolute',
+        top: Dimensions.get('window').height / 2 - dimension / 10 + dimension / 4,
+        right: Dimensions.get('window').width / 2 - dimension / 5 - 4
+    },
+    resumeResetBtn: {
+        borderRadius: dimension / 10,
+        margin: 2
+    },
+    resumeResetBtnTextResume: {
+        color: resumeColor,
+        margin: (dimension / 5 - dimension / 5.5) / 2
+    },
+    resumeResetBtnTextReset: {
+        color: resetColor,
+        margin: (dimension / 5 - dimension / 7) / 2
     }
 });
 
--- components/Chrono.tsx
+++ components/Chrono.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Text, TouchableHighlight } from 'react-native';
+import { Text, TouchableHighlight, View } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import { parseMillis, parseTime } from '../lib/timer_util';
-import styles, { secondaryColor } from './Chrono.styles';
+import styles, { secondaryColor, dimension } from './Chrono.styles';
 import useInterval from '../hooks/useInterval';
 import { useKeepAwake } from 'expo-keep-awake';
 
@@ -30,15 +31,37 @@ const Chrono = () => {
         );
     };
 
-    return (
+    const resumeResetButtons = () => {
+        if(!paused) return <></>;
+        return(
+            <View style={styles.resumeResetContainer}>
+                <TouchableHighlight
+                    onPress={() => setPaused(false)}
+                    style={styles.resumeResetBtn}
+                    underlayColor={secondaryColor}
+                >
+                    <Entypo name="controller-play" size={dimension/5.5} style={styles.resumeResetBtnTextResume} />
+                </TouchableHighlight>
+                <TouchableHighlight
+                    onPress={() => setTimer(0)}
+                    style={styles.resumeResetBtn}
+                    underlayColor={secondaryColor}
+                >
+                    <Entypo name="ccw" size={dimension/7} style={styles.resumeResetBtnTextReset} />
+                </TouchableHighlight>
+            </View>
+        );
+    }
+
+    return (<>
         <TouchableHighlight 
-            onPress={() => setPaused((isPaused) => !isPaused)}
-            onLongPress={reset}
-            style={styles.chronoContainer}
+            onPress={() => setPaused(true)}
+            style={[styles.chronoContainer, {backgroundColor: paused ? secondaryColor : '#ffffff'}]}
             underlayColor={secondaryColor}
         >
             {chronoContent()}
         </TouchableHighlight>
+        {resumeResetButtons()}</>
     );
 }
 
